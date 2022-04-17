a02f293961d1072c0d6c73c09eff3858ae74d430
Andrei Banc <andrei.banc00@gmail.com>
2022-04-17 17:28:23 +0300

    first commit

---
+++ encoder/js/baseband-codes.js
@@ -0,0 +1,121 @@
+function getManchesterLevelEncoding(bits) {
+    var result = [];
+    for (var i = 0; i < bits.length; i++) {
+        let symbol = '⚋⚋';
+        if (parseInt(bits[i].value) == 1) symbol = '▁∣▔';
+      //  AMI = "- 0 _" => - = 1; 0 = 0; _ = -1
+        if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '∣▁∣▔';
+        if (parseInt(bits[i].value) == 0) symbol = '▔∣▁';
+        if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '∣▔∣▁';
+        result.push(symbol);
+    }
+    return result;
+}
+
+function getNRZLEncoding(bits) {
+  var result = [];
+  for (var i = 0; i < bits.length; i++) {
+    let symbol = '';
+    if (parseInt(bits[i].value) == 1) symbol = '▔▔';
+    if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '▔▔';
+    if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '|▔▔';
+    if (parseInt(bits[i].value) == 0) symbol = '▁▁';
+    if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 0) symbol = '▁▁';
+    if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▁▁';
+    result.push(symbol);
+  }
+  return result;
+}
+
+function getNRZMEncoding(bits) {
+  var result = [];
+  var up = false;
+  
+  for (var i = 0; i < bits.length; i++) {
+    let symbol = '';
+    if (parseInt(bits[i].value) == 1) up = !up;
+    if (up) {
+      if (parseInt(bits[i].value) == 0) symbol = '▔▔';
+      if (parseInt(bits[i].value) == 1) symbol = '▔▔';
+      if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▔▔';
+      if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▔▔';
+    }
+    else {
+      if (parseInt(bits[i].value) == 0) symbol = '▁▁';
+      if (parseInt(bits[i].value) == 1) symbol = '▁▁';
+      if (parseInt(bits[i].value) == 0 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▁▁';
+      if (parseInt(bits[i].value) == 1 && i > 0 && parseInt(bits[i - 1].value) == 1) symbol = '|▁▁';
+    }
+    result.push(symbol);
+  }
+  return result;
+}
+
+function getNRZ(bits) {
+  var result = [];
+  var prev = '▁';
+
+  for (var i = 0; i < bits.length; i++) {
+      let symbol = '---';
+
+      if (parseInt(bits[i].value) == 1) {
+          if (prev == '▁') {
+              symbol = '∣▔▔';
+              prev = '▔';
+          } else {
+              symbol = '∣▁▁';
+              prev = '▁';
+          }
+      } else {
+          if (prev == '▁')
+              symbol = '▁▁';
+          else
+              symbol = '▔▔';
+      }
+      result.push(symbol);
+  }
+  return result;
+}
+
+function getNRZS(bits) {
+  var result = [];
+  var prev = '▁';
+  
+  for (var i = 0; i < bits.length; i++) {
+      let symbol = '⚋⚋';
+
+      if (parseInt(bits[i].value) == 0 && (prev == '▁')) {
+          symbol = '∣▔▔';
+          prev = '▔';
+      }
+      else if (parseInt(bits[i].value) == 1 && (prev == '▔'))
+          symbol = '▔▔';
+      else if (parseInt(bits[i].value) == 0 && (prev == '▔')) {
+          symbol = '∣▁▁';
+          prev = '▁';
+      }
+      else if (parseInt(bits[i].value) == 1 && (prev == '▁')) {
+          symbol = '▁▁';
+      }
+
+      result.push(symbol);
+  }
+
+  return result;
+}
+
+function getRZ(bits) {
+  var result = [];
+
+  for (var i = 0; i < bits.length; i++) {
+      let symbol = '⚋⚋';
+      if (parseInt(bits[i].value) == 1) {
+          symbol = '▁∣▔∣';
+      } else {
+          symbol = '▁▁';
+      }
+      result.push(symbol);
+  }
+
+  return result;
+}
